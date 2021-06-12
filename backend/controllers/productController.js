b065c19dffd7ee1339c5d121e291deb857e7c211
Caio <caionwebdev@gmail.com>
2021-06-12 11:31:59 -0300

    adding seeders to the MongoDB

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -12,9 +12,13 @@ exports.newProduct = async (req,res, next) => {
 }
 
 
-exports.getProducts = (req,res,next) => {
+// get all products => /api/v1/products
+exports.getProducts = async (req,res,next) => {
+
+    const products = await Product.find();
     res.status(200).json({
         success: true,
-        message: "this route will show all products in database."
+        count: products.length,
+        products
     })
 } 
