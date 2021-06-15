ae8a76d315f1c5acdd7aabe2cd257c85c99c6bac
Caio <caionwebdev@gmail.com>
2021-06-15 17:02:16 -0300

    adding error handlers

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 
+const errorMiddleware = require("./middlewares/errors")
+
 app.use(express.json())
 
 //import all routes
@@ -9,5 +11,7 @@ const products = require("./routes/product")
 
 app.use("/api/v1", products)
 
+// middleware to handle errors
+app.use(errorMiddleware)
 
 module.exports = app
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require('../models/product')
+const ErrorHandler = require('../utils/errorHandler')
 
-// Create new product => /api/v1/product/new
+// Create new product => /api/v1/admin/product/new
 exports.newProduct = async (req,res, next) => {
 
     const product = await Product.create(req.body)
@@ -22,3 +23,64 @@ exports.getProducts = async (req,res,next) => {
         products
     })
 } 
+
+//get single product details => /api/v1/product/:id
+
+exports.getSingleProduct = async (req,res, next) => {
+
+    const product = await Product.findById(req.params.id)
+
+    if(!product) {
+        return next(new ErrorHandler('Product not found', 404))
+    }
+    
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+
+}
+
+// update product => /api/v1/admin/product/:id
+exports.updateProduct = async (req,res,next) => {
+    let product = await Product.findById(req.params.id)
+
+    if(!product) {
+        return res.status(404).json({
+            success: false,
+            message: "product not found"
+        })
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    }) 
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
+// Delete product => /api/v1/admin/product/:id
+exports.deleteProduct = async (req,res, next) => {
+
+    const product = await Product.findById(req.params.id)
+
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: "Product not found"
+
+        })
+    }
+
+    await product.remove()
+
+    res.status(200).json({
+        success: true,
+        message: "Product is deleted"
+    })
+}
